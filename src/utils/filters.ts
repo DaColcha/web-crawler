8d6db636283bcea8ba509232bb37beb72d102445
Alejandra Colcha <da.colcha@hotmail.com>
2025-10-16 23:22:57 -0500

    add: filters functions

---
+++ src/utils/filters.ts
@@ -0,0 +1,17 @@
+import HackerNewsItem  from '../types/index.ts';
+
+function countWords(text: string): number {
+    const words = text.match(/\b[0-9A-Za-z]+(?:['-.][0-9A-Za-z]+)*\b/g);
+    return words ? words.length : 0;
+}
+
+function wordsLessThan(items: HackerNewsItem[], limit: number): HackerNewsItem[] {
+    return items.filter(item => countWords(item.title) <= limit);
+}
+
+function wordsMoreThan(items: HackerNewsItem[], limit: number): HackerNewsItem[] {
+    return items.filter(item => countWords(item.title) > limit);
+}
+
+export { wordsLessThan, wordsMoreThan, countWords };
+
