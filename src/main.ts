5f691fa7bf578692e6fc2b276d819f43f4726a38
DaColcha <da.colcha@hotmail.com>
2025-10-18 00:10:59 -0500

    add: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { crawlerRouter } from './routes/crawler.route.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/', crawlerRouter);
@@ -10,3 +13,4 @@ app.use('/', crawlerRouter);
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
+
